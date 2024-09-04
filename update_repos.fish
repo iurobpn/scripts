5742496cc5e13d07628bb1d4772e50c251355082
Iuro Nascimento <iurobpn@proton.me>
2024-09-04 18:01:05 -0300

    small fix

--- update_repos.fish
+++ update_repos.fish
@@ -10,20 +10,22 @@ function check_repos
         return 0
     end
 
+	set -l GIT
     if set -q _flag_g
         set GIT $_flag_g
     else
         set GIT $HOME/git
     end
 
+	set -l old_repos
     if test -n "$argv"
         set old_repos $argv
     else
         set old_repos CGAL-matlab ProVANT-Simulator_Developer armadillo-pmr cpp_tests data_structures dotfiles matlab-dev nmpc-obs ocpsol pres-research prov_sim_configs pylattes reports papers scripts sets-obs svgs thesis
         # set repos (echo ""$repos |  sed -e 's#\([a-zA-Z\._0-9\-]\+\)#/home/gagarin/git/\1#g')
     end
-    set n (count $old_repos)
-    set repos
+    set -l n (count $old_repos)
+    set -l repos
     echo "Checking repositories:"
     for i in (seq 1 $n)
         if test -d "$GIT/$old_repos[$i]"
@@ -35,13 +37,13 @@ function check_repos
     echo 'checking finished'
     echo ''
 
-    set pwd $(pwd)
+    set -l pwd $(pwd)
 
     if set -q _flag_r_
-        set dirs
+        set -l dirs
 
-        set dir
-        set n (count $repos)
+        set -l dir
+        set -l n (count $repos)
         for i in (seq 1 $n)
             if not test -d $repos[$i]
                 set -e repos[$i]
@@ -50,7 +52,7 @@ function check_repos
             end
         end
     else
-        set dirs $(fd . -td $GIT -d1)
+        set -l dirs $(fd . -td $GIT -d1)
     end
 
     if test -z "$repos"
@@ -60,7 +62,7 @@ function check_repos
         echo "Found "$(count $repos)" repositories"
     end
 
-    set dirty
+    set -l dirty
     for repo in $repos
         cd $repo
 
@@ -73,7 +75,7 @@ function check_repos
 
         git fetch origin --quiet
         set -l bstatus (branch_status)
-        set current_branch (echo $bstatus | get_local_branch)
+        set -l current_branch (get_current_branch)
         for line in $bstatus
             # echo "-------------- branch status loop ---------------------------"
             set -l nahead (echo $line | get_n_ahead)
@@ -104,12 +106,12 @@ function check_repos
                         sync_repo pull origin $local_branch
                     end
                 else 
-                    set repo_name $(basename $repo)
+                    set -l repo_name $(basename $repo)
 
                     if notstaged || untracked
                         echo ''
                         echo "repo $repo_name has uncommited changes"
-                        set -a dirty $dirty
+                        set -al dirty $dirty
                     end
                 end
                 if test $nahead -gt 0
@@ -147,22 +149,27 @@ function sync_repo
         echo "Usage: sync_repo <command> <remote> <repo>"
         return 1
     end
-    set current_branch (get_current_branch)
-    set cmd $argv[1]
-    set branch $argv[3]
-    set remote $argv[2]
-    if $current_branch != $branch
-        git checkout --quiet $branch
+    set -l current_branch (get_current_branch)
+    set -l cmd $argv[1]
+    set -l branch $argv[3]
+    set -l remote $argv[2]
+	echo "current branch: $current_branch"
+	echo "branch: $branch"
+	echo "remote: $remote"
+	echo "cmd: $cmd"
+
+    if "$current_branch" != "$branch"
+        git checkout $branch --quiet
     end
     git $cmd $remote $branch
-    if $current_branch != $branch
+    if "$current_branch" != "$branch"
         git checkout --quiet -
     end
 end
 
 function update_ufmg
-    set GIT $HOME/git
-    set repos CGAL-matlab ProVANT-Simulator_Developer armadillo-pmr cpp_tests data_structures dotfiles matlab-dev nmpc-obs ocpsol pres-research prov_sim_configs pylattes reports papers scripts sets-obs svgs thesis
+    set -l GIT $HOME/git
+    set -l repos CGAL-matlab ProVANT-Simulator_Developer armadillo-pmr cpp_tests data_structures dotfiles matlab-dev nmpc-obs ocpsol pres-research prov_sim_configs pylattes reports papers scripts sets-obs svgs thesis
     # set repos (echo $repos |  sed -e 's#\([a-zA-Z\._0-9\-]\+\)#/home/gagarin/git/\1#g')
     # echo "updating repos: $repos"
     check_repos $repos
