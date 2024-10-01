a8af14032a9a41bf5a6d036ef209c0d02ef8d16a
Iuro Nascimento <iurobpn@proton.me>
2024-10-01 20:26:37 -0300

    fixing runner plugin

--- lua/dev/nvim/runner.lua
+++ lua/dev/nvim/runner.lua
@@ -4,25 +4,68 @@
 
 local M = {}
 -- include directions for new panes
-function M.run_command(...)
-    local opts = {...}
-    local is_nil = opts[1] == nil
-    opts = opts[1] or {}
-    if type(opts) == 'string' or is_nil then
-        opts = {cmd = opts, win_type = 'pane', dir = 'left'}
-    end
-    if opts.win_type == 'float' then
-        vim.cmd("!zellij run -c -f -- " .. opts.cmd)
-    elseif is_nil or opts.win_type == 'pane' then
-        vim.cmd("!zellij run -c -d " .. opts.dir .. ' -- ' .. opts.cmd)
+function M.run_command(opts)
+    local z_opts = {cmd = '', args = '', win_type = 'pane', dir = 'left'}
+
+
+                -- z_opts.args = ' --floating'
+    -- the arguments before a '--' are options for the command
+    -- if no '--' is present, the command is assumed to be a pane command
+    local res = opts.args:match('%-%-')
+    if res ~= nil then
+        print('matched: fargs: ', vim.inspect(opts.fargs))
+        for i, opt in ipairs(opts.fargs) do
+            if opt == '--' then
+                z_opts.cmd =  table.concat(opts.fargs, ' ', i+1)
+                break
+            end
+            if opt == 'float' then
+                z_opts.args = z_opts.args .. ' --floating'
+            elseif opt == 'close-on-exit' then
+                z_opts.args = z_opts.args .. ' --close-on-exit'
+            elseif  opt == 'right' or opt == 'left' or opt == 'up' or opt == 'down' then
+                z_opts.args = z_opts.args .. ' --direction ' .. opt
+            elseif opt == 'in-place' then
+                z_opts.args = z_opts.args .. ' --' .. opt
+            elseif opt == 'start-suspended' then
+                z_opts.args = z_opts.args .. ' --start-suspended'
+            else
+                print('Invalid option: ' .. opt)
+            end
+        end
     else
-        vim.cmd("!zellij run -c -d left -- " .. opts.cmd)
+        print('it did not match: ' .. opts.args)
+        z_opts.cmd = opts.args
+    end
+    local cmd = 'zellij run ' .. z_opts.args .. ' -- ' .. z_opts.cmd
+    -- if #z_opts.args > 0 then
+        -- cmd = cmd .. ' -- '  .. z_opts.cmd
+    -- else
+    --     cmd = cmd .. ' ' .. z_opts.cmd
+    -- end
+
+    print('cmd: ', cmd)
+    local err, msg = pcall(os.execute, opts.cmd )
+    if err then
+        print('Error running command: ' .. msg)
+    else
+        print('Command ran successfully with: ', msg)
     end
 end
 
 -- create the command
-vim.api.nvim_create_user_command("Run", "lua require'dev.nvim.runner'.run_command(<args>)", {
-    nargs = "+",
-})
+vim.api.nvim_create_user_command("Run", function(opts) M.run_command(opts) end, {nargs = "+", bang = true, desc = 'run a command iwith zellij in the current pane or a new pane (float or aside)'})
+
+vim.api.nvim_create_user_command(
+    'MyCommand',           -- Command name
+    function(opts)         -- Function to execute
+        -- Handle arguments here
+        print('Arguments received:')
+        for i, arg in ipairs(opts.fargs) do
+            print(string.format('Argument %d: %s', i, arg))
+        end
+    end,
+    { nargs = '+' }        -- '+' means one or more arguments required
+)
 
 return M
