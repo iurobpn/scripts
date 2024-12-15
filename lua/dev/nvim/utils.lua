8bf3e891af4c7f02ccb30e9e5803dfedf46a8e11
Iuro Nascimento <iurobpn@proton.me>
2024-12-15 10:52:45 -0300

    add type hints to utils lua module

--- lua/dev/nvim/utils.lua
+++ lua/dev/nvim/utils.lua
@@ -1,6 +1,11 @@
 local utils = require('utils')
 local M = {}
 
+---get file:line context
+---@param filename any
+---@param line_num any
+---@param size any
+---@return string[]|nil
 function M.get_context(filename, line_num, size)
     if not filename then
         print('Error: filename is required to get the file context')
@@ -29,11 +34,14 @@ function M.get_context(filename, line_num, size)
     local last_line = vim.api.nvim_buf_line_count(bufnr);
     local start_line = math.max(0, line_num - context_lines_before - 1)
     local end_line = math.min(last_line, line_num + context_lines_after)
-    local lines  = vim.api.nvim_buf_get_lines(bufnr, start_line, end_line, false)
+    local lines  = Buffer.get_lines(bufnr, start_line, end_line)
 
     return lines
 end
 
+--- is_file_loaded
+---@param filepath string
+---@return boolean
 function M.is_file_loaded(filepath)
     -- Get the buffer number for the file (returns -1 if the file is not loaded)
     local bufnr = vim.fn.bufnr(filepath)
@@ -46,6 +54,12 @@ function M.is_file_loaded(filepath)
     return false
 end
 Buffer = {}
+
+--- test if a map exists
+---@param bufnr number
+---@param mode characters
+---@param lhs string
+---@return boolean
 function Buffer.mapping_exists(bufnr, mode, lhs)
     local mappings = vim.api.nvim_buf_get_keymap(bufnr, mode)
     for _, map in ipairs(mappings) do
@@ -56,7 +70,8 @@ function Buffer.mapping_exists(bufnr, mode, lhs)
     return false
 end
 
-
+--- remove a mapping
+---@param bufnr integer
 function Buffer.unmap(bufnr)
     local modes = {'n', 'v', 'i', 'x', 's', 'o', 'c', 't'}
     for _,mode in ipairs(modes) do
@@ -67,9 +82,19 @@ function Buffer.unmap(bufnr)
     end
 end
 
+--- number of lines 
+---@param buf number
+---@return integer
+function Buffer.size(buf)
+    return vim.api.nvim_buf_line_count(buf)
+end
+
+---load a file into a buffer
+---@param buf integer
+---@param filename string
 function Buffer.load(buf,filename)
     if buf ~= nil or not Buffer.is_valid(buf) then
-        print("No buffer provided")
+        vim.notify('Buffer is not valid')
         return
     end
     if not filename or #filename == 0 then
@@ -77,12 +102,17 @@ function Buffer.load(buf,filename)
         return
     end
     vim.api.nvim_buf_set_name(buf, filename)
-    vim.api.nvim_command("edit " .. filename)
+    -- read the file using readfile builtin
+    local lines = vim.fn.readfile(filename)
+    Buffer.set_lines(buf, lines)
 end
 
+---append content to a buffer
+---@param buf integer
+---@param content string[]|string
 function Buffer.append(buf, content)
     if not Buffer.is_valid(buf) then
-        print("No buffer provided")
+        vim.notify('Buffer is not valid')
         return
     end
     local line_start = vim.api.nvim_buf_line_count(buf)
@@ -91,13 +121,13 @@ function Buffer.append(buf, content)
 end
 
 ---set lines in buffer
----@param buf number
----@param line_start number
----@param line_end number
----@param content string
+---@param buf integer
+---@param line_start integer
+---@param line_end integer
+---@param content string[]|string
 function Buffer.set_lines(buf, content, line_start, line_end, strict)
-    if buf ~= nil or not Buffer.is_valid(buf) then
-        print("No buffer provided")
+    if buf == nil or not Buffer.is_valid(buf) then
+        vim.notify('Buffer is not valid')
         return
     end
     if line_start == nil then
@@ -113,6 +143,9 @@ function Buffer.set_lines(buf, content, line_start, line_end, strict)
     vim.api.nvim_buf_set_lines(buf, line_start, line_end, strict, content) -- overwrite file
 end
 
+---ensures the content is properly wrapped into a table
+---@param content string[]|string
+---@return string[]
 function Buffer.check_content(content)
     if type(content) == 'string' then
         content = vim.split(content, '\n')
@@ -123,31 +156,35 @@ function Buffer.check_content(content)
     return content
 end
 
--- preserve marks
+---wraps nvim_buf_set_text
+---@param buf integer
+---@param start_row integer
+---@param start_col integer
+---@param end_row integer
+---@param end_col integer
+---@param content string[]
 function Buffer.set_text(buf, start_row, start_col, end_row, end_col, content)
     content = Buffer.check_content(content)
     vim.api.nvim_buf_set_text(buf, start_row, start_col, end_row, end_col, content)
 end
 
-function Buffer.new(listed, scratch)
-    if listed == nil then
-        listed = true
-    end
-    if scratch == nil then
-        scratch = false
-    end
-    return vim.api.nvim_create_buf(listed, scratch)  -- false for listed, true for scratch
+
+---creates a new lieted buffer
+---@return integer
+function Buffer.new()
+    return vim.api.nvim_create_buf(true, false)  -- false for listed, true for scratch
 end
 
--- add acratch buffer
+---comment
+---@param content any
+---@return integer
 function Buffer.scratch(content)
-    local buf = Buffer.new(false, true)
-    Buffer.set_lines(buf, content)
-    return buf
+    return vim.api.nvim_create_buf(false, true)  -- false for listed, true for scratch
 end
 
 function Buffer.delete(buf)
     if not Buffer.is_valid(buf) then
+        vim.notify('Buffer is not valid')
         return
     end
     vim.api.nvim_buf_delete(buf, {force = true})
@@ -155,6 +192,7 @@ end
 
 function Buffer.get_lines(buf, start, finish, strict)
     if not Buffer.is_valid(buf) then
+        vim.notify('Buffer is not valid')
         return
     end
     if not start then
@@ -164,13 +202,14 @@ function Buffer.get_lines(buf, start, finish, strict)
         finish = -1
     end
     if strict == nil then
-        strict = true
+        strict = false
     end
     return vim.api.nvim_buf_get_lines(buf, start, finish, strict)
 end
 
 function Buffer.get_text(buf, start_row, start_col, end_row, end_col)
     if not Buffer.is_valid(buf) then
+        vim.notify('Buffer is not valid')
         return
     end
     return vim.api.nvim_buf_get_text(buf, start_row, start_col, end_row, end_col)
@@ -178,6 +217,7 @@ end
 
 function Buffer.get_name(buf)
     if not Buffer.is_valid(buf) then
+        vim.notify('Buffer is not valid')
         return
     end
     return vim.api.nvim_buf_get_name(buf)
@@ -185,6 +225,7 @@ end
 
 function Buffer.get_option(buf, option)
     if not Buffer.is_valid(buf) then
+        vim.notify('Buffer is not valid')
         return
     end
     return vim.api.nvim_get_option_value(option , { buf = buf})
@@ -192,6 +233,7 @@ end
 
 function Buffer.set_option(buf, option, value)
     if not Buffer.is_valid(buf) then
+        vim.notify('Buffer is not valid')
         return
     end
     vim.api.nvim_set_option_value(option, value, { buf = buf } )
