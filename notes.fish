5fd14857697ea4320e93f69d013ca9a77d1f5f52
Iuro Nascimento <iurobpn@proton.me>
2025-02-12 20:16:46 -0300

    fix notes function

--- notes.fish
+++ notes.fish
@@ -1,4 +1,10 @@
 function notes
+    if [ $argv[1] == "-h" ]
+        echo "Usage: notes [notes_dir]"
+        echo "Open a new zellij tab with the notes layout."
+        echo "If notes_dir is not provided, it defaults to $HOME/sync/obsidian."
+        return
+    end
     if test -z "$argv"
         set notes_dir $HOME/sync/obsidian
     else
@@ -9,7 +15,7 @@ function notes
     if zellij action query-tab-names | grep notes
         zellij action go-to-tab-name notes
     else
-        zellij action new-tab --layout notes.kdl --name notes --cwd "$NOTES_DIR"
+        zellij action new-tab --layout notes --name notes --cwd "$NOTES_DIR"
     end
 end
 
