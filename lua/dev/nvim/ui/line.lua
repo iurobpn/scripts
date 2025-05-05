a0bee2280a8612a1214e3c2e251458c95c552668
Iuro Nascimento <iurobpn@proton.me>
2025-05-05 19:37:41 -0300

    nui examples

---
+++ lua/dev/nvim/ui/line.lua
@@ -0,0 +1,10 @@
+local NuiLine = require("nui.line")
+
+local line = NuiLine()
+
+line:append("Something Went Wrong!", "Error")
+
+local bufnr, ns_id, linenr_start = 0, -1, 1
+
+line:render(bufnr, ns_id, linenr_start)
+
