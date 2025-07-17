4faf96252d98ab3d37756359edf3d8bcbdc8a772
Iuro Nascimento <iurobpn@proton.me>
2025-07-17 18:28:45 -0300

    nix tests

---
+++ nix/tmux/tmux.nix
@@ -0,0 +1,15 @@
+# tmux.nix
+{
+    stdenv,
+    fetchzip,
+}:
+
+stdenv.mkDerivation {
+    pname = "Hello";
+    version = "2.12.1";
+
+    src = fetchzip {
+        url = "https://ftp.gnu.org/gnu/hello/hello-2.12.1.tar.gz";
+        sha256 = "sha256-1kJjhtlsAkpNB7f6tZEs+dbKd8z7KoNHyDHEJ0tmhnc=";
+    };
+}
